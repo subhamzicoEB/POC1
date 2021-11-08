bce8ad141896de6f0895eb3ad6a793c007b51646
Paul <spaul5@wm.com>
2021-11-09 02:43:32 +0530

    First Commit

--- lib/project-2-stack.ts
+++ lib/project-2-stack.ts
@@ -1,15 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class Project2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Project2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'MyFirstBucket', {
+      bucketName : 'testbucketfordevops'
+    })
+   
   }
 }
